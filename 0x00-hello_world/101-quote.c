2b9146382b8c70e38e73bf7965c57be3e237fe10
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-16 03:55:43 -0800

    and that piece

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (0);
+}
+
