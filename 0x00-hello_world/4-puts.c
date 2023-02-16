30f7eac38f624db287a7e4cdc0d671024c20eae7
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-16 02:43:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C program that display a line using the put function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
