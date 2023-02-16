97a71823819fe8ec41654789fdec516055f5c418
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-16 02:54:50 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line in C using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
