e7132bd63c4ae21cf3be719808bc7ba5eaf3fd29
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-16 04:03:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (0);
 }
 
