d18895c353188fb6573ed3bba01759bac0f43b39
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-17 08:57:50 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+return (0);
+}
