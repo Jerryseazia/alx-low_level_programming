205738bd807fb8927d94ce65fa74a689943b92e0
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-17 08:18:33 -0800

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 x = n % 10;
 if (x > 5)
 {
-printf("Last digit of %d is %d greater than 5\n", n, x);
+printf("Last digit of %i is %i and is greater than 5\n", n, x);
 }
 else if (x == 0)
 {
-printf("Last digit of %d is %d and 0\n", n, x);
+printf("Last digit of %i is %i and is 0\n", n, x);
 }
-else if (x < 6)
+else if ((x < 6) && (x > 0))
 {
-printf("Last digit of %d is %d and is less than 6\n", n, x);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, x);
 }
 return (0);
 }
