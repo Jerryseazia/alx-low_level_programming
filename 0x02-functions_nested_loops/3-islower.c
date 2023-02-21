5e7fd6c1d9ae6d57b5c000d5bd1ede586bea5c86
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-21 12:10:32 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ * @c: the value to be checked
+ * Return: 1 if true and 0 if not
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
