38c2b533fede09dfd98540f1db6aeadb1fd1443f
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-21 12:43:07 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,14 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n < 0)
+else if (n == 0)
 {
-_putchar('-');
+_putchar(0 + '\0');
 return (0);
 }
 else
 {
-_putchar(0 + '0');
-return (0);
+_putchar('-');
+return (-1);
 }
 }
