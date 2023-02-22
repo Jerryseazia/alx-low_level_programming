81503f4a5a74d6d68f106be6bdf18a488de1fd1c
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-21 16:54:13 -0800

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,21 +6,38 @@
  */
 int main(void)
 {
-int i;
-unsigned long int j, k, next, sum;
-j = 1;
-k = 2;
-sum = 0;
-for (i = 1; i <= 33; ++i)
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+for (count = 0; count < 92; count++)
 {
-if (j < 4000000 && (j % 2) == 0)
+sum = fib1 + fib2;
+printf("%lu, ", sum);
+fib1 = fib2;
+fib2 = sum;
+}
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
+for (count = 93; count < 99; count++)
 {
-sum = sum + j;
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
 }
-next = j + k;
-j = k;
-k = next;
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
 }
-printf("%lu\n", sum);
+printf("\n");
 return (0);
 }
