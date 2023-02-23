e1fbc89bd43ad8622a65ace4d87d93c9a1b5bc1e
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-23 12:00:44 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - numbers 0 - 9
+ * @c: it is an int
+ *
+ * Return: either 1 0r 0
+ */
+int _isdigit(int c)
+{
+if (c > 47 && c < 58)
+{
+return (1);
+}
+return (0);
+}
