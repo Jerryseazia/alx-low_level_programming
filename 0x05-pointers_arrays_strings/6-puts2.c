02146c6d44cccec82bb89bf171f6fe8e1d6929c2
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-02-27 10:44:00 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every  other character of a string.
+ *@str: the string to use.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
