0b0d4d58271d1a10bbe2f7fccc7fefa0b38b5150
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-03-22 14:55:46 -0700

    task 0

---
+++ 0x0F-function_pointers/0-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+/**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_uppercase(char *name)
+{
+unsigned int i;
+printf("Hello, my uppercase name is ");
+i = 0;
+while (name[i])
+{
+if (name[i] >= 'a' && name[i] <= 'z')
+{
+putchar(name[i] + 'A' - 'a');
+}
+else
+{
+putchar(name[i]);
+}
+i++;
+}
+}
---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function to print name
+ *
+ * @name: name to print
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
