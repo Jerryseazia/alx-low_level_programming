531bfe7ac103f6368c15cd4478f67b5219abccc9
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-05-18 16:49:14 -0700

    number 3

--- 0x17-doubly_linked_lists/3-main.c
+++ 0x17-doubly_linked_lists/3-main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
  * main - check the code
  *
@@ -9,16 +10,17 @@
  */
 int main(void)
 {
- dlistint_t *head;
-head = NULL;
-add_dnodeint_end(&head, 0);
-add_dnodeint_end(&head, 1);
-add_dnodeint_end(&head, 2);
-add_dnodeint_end(&head, 3);
-add_dnodeint_end(&head, 4);
-add_dnodeint_end(&head, 98);
-add_dnodeint_end(&head, 402);
-add_dnodeint_end(&head, 1024);
-print_dlistint(head);
-return (EXIT_SUCCESS);
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    return (EXIT_SUCCESS);
 }
