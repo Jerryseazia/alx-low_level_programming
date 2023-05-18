018ba6b919d065091f69ed74bf6e7a9df388c3cf
Jerry Seazia <jerrykoiyanseazia@gmail.com>
2023-05-18 16:46:42 -0700

    new commit1

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,32 +1,33 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
- * add_dnodeint_end - adds a new node at the end
- * of a dlistint_t list
- *
- * @head: head of the list
- * @n: value of the element
- * Return: the address of the new element
+ * add_dnodeint_end - function of the prototype
+ * @head: pointer to head of list
+ * @n: node data
+ * Return: address of new node, or Null
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *h;
-dlistint_t *new;
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
+dlistint_t *recent;
+dlistint_t *end;
+if (!head)
 return (NULL);
-new->n = n;
-new->next = NULL;
-h = *head;
-if (h != NULL)
+recent = malloc(sizeof(struct dlistint_s));
+if (!recent)
+return (NULL);
+recent->n = n;
+if (*head == NULL)
 {
-while (h->next != NULL)
-h = h->next;
-h->next = new;
+*head = recent;
+recent->next = NULL;
+recent->prev = NULL;
+return (recent);
 }
-else
-{
-*head = new;
-}
-new->prev = h;
-return (new);
+end = *head;
+while (end->next != NULL)
+end = end->next;
+recent->next = NULL;
+recent->prev = end;
+end->next = recent;
+return (recent);
 }
